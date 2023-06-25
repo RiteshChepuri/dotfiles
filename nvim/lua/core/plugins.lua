10afb0cfcde1d88b13300fe16bf4a515d816cc1e
RiteshChepuri <riteshchepuri0@gmail.com>
2023-06-25 09:43:53 +0530

    cleanup

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -1,5 +1,4 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim" if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim" if not vim.loop.fs_stat(lazypath) then vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
@@ -122,7 +121,7 @@ local plugins = {
       stages = "static",
       timeout = 1500,
     },
-  },
+  }
 }
 
 local opts = {}
