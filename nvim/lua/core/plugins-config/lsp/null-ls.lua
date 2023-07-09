80a12773949981a817c556bf9675b722e4167a1f
RiteshChepuri <riteshchepuri0@gmail.com>
2023-07-09 17:57:49 +0530

    configured for formatter and diagnostics(css,c,java,lua,markdown.json,js,ts,spellcheck,python)

---
+++ nvim/lua/core/plugins-config/lsp/null-ls.lua
@@ -0,0 +1,69 @@
+-- import null-ls plugin safely
+local setup, null_ls = pcall(require, "null-ls")
+if not setup then
+	return
+end
+
+-- for conciseness
+local formatting = null_ls.builtins.formatting -- to setup formatters
+local diagnostics = null_ls.builtins.diagnostics -- to setup linters
+local hover = null_ls.builtins.hover -- to setup hover
+
+-- to setup format on save
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+-- configure null_ls
+null_ls.setup({
+	-- setup formatters & linters
+	sources = {
+		--  to disable file types use
+		--  "formatting.prettier.with({disabled_filetypes = {}})" (see null-ls docs)
+
+		hover.dictionary,
+
+		-- Formatters
+		formatting.prettier, -- js/ts formatter
+		formatting.prettierd, -- js/ts formatter advanced
+		formatting.stylua, -- lua formatter
+		formatting.astyle, -- c, java  formatter
+		formatting.black, -- Python formatter
+		formatting.codespell, -- Codespell in files formatter
+		formatting.eslint_d, -- js, jsreact,ts,tsreact formatter
+		formatting.fixjson, -- JSON formatter
+		formatting.google_java_format, -- Java formatter
+
+		-- Diagnostics
+		diagnostics.stylelint, -- css,
+		diagnostics.cpplint, -- c and c++
+		diagnostics.checkstyle, -- java
+		diagnostics.luacheck, -- lua
+		diagnostics.alex, -- markdown
+		diagnostics.flake8, -- python linter
+		diagnostics.cspell, --Spell checker linter
+		diagnostics.eslint_d.with({ -- js/ts linter
+			-- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
+			condition = function(utils)
+				return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
+			end,
+		}),
+	},
+	-- configure format on save
+	on_attach = function(current_client, bufnr)
+		if current_client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({
+						filter = function(client)
+							--  only use null-ls for formatting instead of lsp server
+							return client.name == "null-ls"
+						end,
+						bufnr = bufnr,
+					})
+				end,
+			})
+		end
+	end,
+})
