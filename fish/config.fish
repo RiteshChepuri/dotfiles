9151e9a97d96729f4411fcb6e7d2f72c4267bf35
Ritesh Chepuri <riteshchepuri0@gmail.com>
2023-11-05 17:30:54 +0530

    feat: added fish shell essentials

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Disable fish greeting
+set -Ux fish_greeting
+
+# Set Neovim as default editor
+set -Ux EDITOR nvim
+
