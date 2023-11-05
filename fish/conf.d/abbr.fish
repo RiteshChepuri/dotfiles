6f9690156a472b3569b00cb77a195503bae4e087
Ritesh Chepuri <riteshchepuri0@gmail.com>
2023-11-05 17:42:48 +0530

    feat: added brew related abbrs

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -8,3 +8,11 @@ abbr v "nvim"
 abbr v. "nvim +GoToFile"
 abbr :GoToFile "nvim +GoToFile"
 abbr :GoToCommand "nvim +GoToCommand"
+
+# Brew
+abbr bc "brew cleanup"
+abbr bd "brew doctor"
+abbr bi "brew install"
+abbr bo "brew outdated"
+abbr bu "brew update"
+abbr bug "brew upgrade"
