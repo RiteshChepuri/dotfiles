15f56b1b47a905e661592f0359d774d0364c1859
RiteshChepuri <riteshchepuri0@gmail.com>
2023-11-13 22:31:13 +0530

    feat: added vim options

---
+++ nvim/lua/core/options.lua
@@ -0,0 +1,64 @@
+-- For Consciseness
+local o = vim.opt
+
+-- Line Numbers
+o.relativenumber = true
+o.number = true
+o.numberwidth = 1
+
+-- Tabs and Indentation
+o.tabstop = 2 -- 2 spaces for tabs
+o.shiftwidth = 2 -- 2 spaces for indent width
+o.expandtab = true -- Expand tab to Spaces
+o.softtabstop = 4 -- Setting tab to 4 while typing
+o.smartindent = true -- Insert indents automatically
+
+-- Line wrapping
+o.wrap = false
+
+-- Search Settings
+o.ignorecase = true -- Ignore case while searching
+o.smartcase = true -- If you include mixed case in your search, it assumes you want case sesitive search
+o.hlsearch = true -- Highlight search results
+o.incsearch = true -- Set Incremental Search like modern browsers
+
+-- Cursor Line
+o.cursorline = false -- Highlight cursor line
+
+-- Appearence
+o.termguicolors = true
+o.background = "dark" -- Background set to dark which is required for various colorschemes
+o.signcolumn = "yes" -- Show Sign column so that text doesnt shift
+o.winblend = 0 -- setting the winblend to 0
+o.wildoptions = "pum" -- Enabling pumblend
+
+-- Scroll off
+o.scrolloff = 10 -- Scroll off to 10
+o.sidescrolloff = 8 -- Sidescroll off to 8
+
+-- Scrolling
+o.ttyfast = true
+o.lazyredraw = false
+
+-- Command Line
+o.cmdheight = 1
+
+-- Backup
+o.backup = false
+o.swapfile = false
+o.undofile = true
+
+-- Window split
+o.splitright = true
+o.splitbelow = true
+
+-- encoding
+o.encoding = "utf-8"
+o.fileencoding = "utf-8"
+
+-- Language specification
+o.spelllang = { "en" }
+
+-- Enable auto write
+o.autowrite = true
+
