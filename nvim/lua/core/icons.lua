7f467caa11c81fe84e5f146908de8bbf64902374
RiteshChepuri <riteshchepuri0@gmail.com>
2023-11-14 14:35:00 +0530

    feat: added icons for neovim

---
+++ nvim/lua/core/icons.lua
@@ -0,0 +1,16 @@
+local M = {
+	diags = {
+		error = " ",
+		warn = " ",
+		hint = " ",
+		info = " ",
+	},
+	git = {
+		added = "",
+		changed = "",
+		deleted = "",
+		untracked = "",
+	},
+}
+
+return M
