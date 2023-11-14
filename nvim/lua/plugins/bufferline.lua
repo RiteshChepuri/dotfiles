fa983842aed877ab853fb9fe821d78e70b62c1c6
RiteshChepuri <riteshchepuri0@gmail.com>
2023-11-14 10:14:16 +0530

    feat: added bufferline plugin

---
+++ nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,14 @@
+return {
+  "akinsho/bufferline.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  version = "*",
+  opts = {
+    options = {
+      always_show_bufferline = false,
+      show_buffer_close_icons = false,
+      show_close_icon = false,
+      color_icons = true,
+      mode = "buffers",
+    },
+  },
+}
