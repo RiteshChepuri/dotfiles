da96b57df2a4feec0beefc6e069c9bb2a6c5b956
RiteshChepuri <riteshchepuri0@gmail.com>
2023-11-16 17:29:01 +0530

    feat: added solarized osaka colorscheme nvim

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  "craftzdog/solarized-osaka.nvim",
+  lazy = true,
+  prority = 1000,
+  opts = function()
+    return {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    }
+  end,
+}
