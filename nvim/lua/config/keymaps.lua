c8b1004e3a370284157d35e39a8b98033d4b5caf
RiteshChepuri <riteshchepuri0@gmail.com>
2023-11-16 17:34:07 +0530

    feat: added essential keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,37 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local k = vim.keymap -- For Consciseness
+-- General Keymaps
+
+-- Use jj to exit insert mode
+k.set("i", "jj", "<ESC>")
+
+-- Delete a character without copying into the register
+k.set("n", "x", '"_x')
+
+-- Clear searches
+k.set("n", "<ESC>", "<Cmd>noh<CR><ESC>")
+
+-- Scroll Page down (Center the cursor)
+k.set("n", "<C-d>", "<C-d>zz")
+k.set("n", "<C-u>", "<C-u>zz")
+
+-- Disable Space
+k.set("n", "<Space>", "<NOP>")
+
+-- Keep The Search results in middle of the screen
+k.set("n", "n", "nzz")
+k.set("n", "N", "Nzz")
+
+-- Replace the Current word within entire file from ThePrimeagen config
+k.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Open Line above or below
+k.set("n", "<A-o>", "o<ESC>")
+k.set("n", "<A-O>", "O<ESC>")
+
+-- Go to start or end of line easier from jdhao config
+k.set({ "n", "x" }, "H", "^")
+k.set({ "n", "x" }, "L", "g_")
