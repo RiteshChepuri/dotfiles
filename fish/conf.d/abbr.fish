e402e2cf48d80b17923f758e29218f334d0a380d
RiteshChepuri <riteshchepuri0@gmail.com>
2023-11-18 12:44:40 +0530

    feat: added git related abbrs

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -1,10 +1,10 @@
 # Essential
 abbr dc "cd .."
-abbr cls "clear"
+abbr cls clear
 abbr rmf "rm -rf"
 
 # Neovim
-abbr v "nvim"
+abbr v nvim
 abbr v. "nvim +GoToFile"
 abbr :GoToFile "nvim +GoToFile"
 abbr :GoToCommand "nvim +GoToCommand"
@@ -17,7 +17,12 @@ abbr bo "brew outdated"
 abbr bu "brew update"
 abbr bug "brew upgrade"
 
-# TMUX 
+# TMUX
 abbr ta "tmux a"
 abbr tk "tmux kill-server"
 abbr td "tmux detach"
+
+# Git
+abbr gs "git status"
+abbr gd "git diff"
+abbr gl "git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
