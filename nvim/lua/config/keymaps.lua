f8d810f2716c9d513ddc50d6830c80b92af7a05e
RiteshChepuri <riteshchepuri0@gmail.com>
2023-11-18 08:56:52 +0530

    feat: removed keymap for markdown preview (iamcco)

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -81,9 +81,6 @@ end)
 -- Undotree
 k.set("n", "<leader>u", "<Cmd>UndotreeToggle<CR>")
 
--- Markdown preview
--- k.set("n", "<leader>mp", "<Cmd>MarkdownPreview<CR>")
---
 -- Diffview
 k.set("n", "<leader>gd", "<Cmd>DiffviewOpen<CR>")
 k.set("n", "<leader>gq", "<Cmd>DiffviewClose<CR>")
