55b6728e03d29924c207f12c9369982edcd07e75
RiteshChepuri <riteshchepuri0@gmail.com>
2023-12-30 10:30:22 +0530

    feat: added keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,7 +1,3 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
 local k = vim.keymap -- For Consciseness
 -- General Keymaps
 
@@ -36,14 +32,15 @@ k.set("n", "<A-O>", "O<ESC>")
 k.set({ "n", "x" }, "H", "^")
 k.set({ "n", "x" }, "L", "g_")
 
--- Plugins related keymaps
+k.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
+k.set("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
+k.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
+k.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
 
--- Undotree
-k.set("n", "<leader>u", "<Cmd>UndotreeToggle<CR>")
-
--- Diffview
-k.set("n", "<leader>gd", "<Cmd>DiffviewOpen<CR>")
-k.set("n", "<leader>gq", "<Cmd>DiffviewClose<CR>")
+-- Keymaps related PLugins
+--
+-- nvimtree
+k.set("n", "<leader>e","<Cmd>NvimTreeToggle<CR>")
 
 -- Telescope
 local builtin = require("telescope.builtin")
@@ -54,11 +51,30 @@ k.set("n", "<leader>fd", builtin.diagnostics, {})
 k.set("n", "<leader>fk", builtin.keymaps, {})
 k.set("n", "<leader>fb", builtin.buffers, {})
 k.set("n", "<leader>fc", builtin.commands, {})
-k.set("n", "<leader>fh", "<Cmd>Telescope harpoon marks<CR>", {})
-k.set("n", "<leader>gf", builtin.git_files, {})
-k.set("n", "<leader>gc", builtin.git_commits, {})
-k.set("n", "<leader>gb", builtin.git_bcommits, {})
-k.set("n", "<leader>gs", builtin.git_status, {})
+-- k.set("n", "<leader>fh", "<Cmd>Telescope harpoon marks<CR>", {})
+k.set("n", "<leader>gf", builtin.git_files,	 {})
+k.set("n", "<leader>gc", builtin.git_commits,	 {})
+k.set("n", "<leader>gb", builtin.git_bcommits,	 {})
+k.set("n", "<leader>gs", builtin.git_status,	 {})
 
--- Peek (Markdow previewer)
-k.set("n", "<leader>mp", "<Cmd>PeekOpen<CR>", {})
+-- Undotree
+k.set("n", "<leader>u","<Cmd>UndotreeToggle<CR>")
+
+-- markdown preview
+k.set("n", "<leader>mp","<Cmd>MarkdownPreview<CR>")
+
+-- Diffview
+k.set("n", "<leader>gd", "<Cmd>DiffviewOpen<CR>")
+k.set("n", "<leader>gq", "<Cmd>DiffviewClose<CR>")
+
+-- trouble
+k.set("n", "<leader>xx", function() require("trouble").toggle() end)
+k.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
+k.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
+k.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
+k.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
+k.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
+
+-- Trouble
+k.set("n", "<leader>xt","<Cmd>TodoTrouble<CR>")
+k.set("n", "<leader>tt","<Cmd>TodoTelescope<CR>")
