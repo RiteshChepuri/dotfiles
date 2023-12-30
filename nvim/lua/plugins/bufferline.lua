00dafeea2533fd4b4c23f29a3f3af3b928fa2e7a
RiteshChepuri <riteshchepuri0@gmail.com>
2023-12-30 10:31:44 +0530

    feat: added bufferline plugin

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,16 +1,15 @@
-return {
-  "akinsho/bufferline.nvim",
-  event = "VeryLazy",
-  keys = {
-    { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
-    { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+return {"akinsho/bufferline.nvim",
+event = "VeryLazy",
+keys = {
+  { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+  { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+},
+opts = {
+  options = {
+    mode = "buffers",
+    separator_style = "slope",
+    show_buffer_close_icons = false,
+    show_close_icon = false,
+    always_show_bufferline = false,
   },
-  opts = {
-    options = {
-      mode = "buffers",
-      -- separator_style = "slant",
-      show_buffer_close_icons = false,
-      show_close_icon = false,
-    },
-  },
-}
+}}
