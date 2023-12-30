c671b6fa032cd420465022e3d9e0e76a45a252e4
RiteshChepuri <riteshchepuri0@gmail.com>
2023-12-30 10:36:39 +0530

    feat: added vim-tmux-navigator, plenary, vim-startuptime, dressing plugins

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -1,3 +1,12 @@
-return { { "sindrets/diffview.nvim" }, {
-  "wakatime/vim-wakatime",
-} }
+return {
+  {"christoomey/vim-tmux-navigator"},
+  { "nvim-lua/plenary.nvim", lazy = true },
+  {
+    "dstein64/vim-startuptime",
+    cmd = "StartupTime",
+  },
+{
+  'stevearc/dressing.nvim',
+  opts = {},
+}
+}
