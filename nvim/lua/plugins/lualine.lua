cfc334eb5ffea82f3cb9c648353e950f8a73a49e
RiteshChepuri <riteshchepuri0@gmail.com>
2024-01-02 10:06:54 +0530

    chore: removed bufferline, nvim tree, undotree from bufferline config

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,19 +1,4 @@
 return {
-  {"akinsho/bufferline.nvim",
-  event = "VeryLazy",
-  keys = {
-    { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
-    { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
-  },
-  opts = {
-    options = {
-      mode = "buffers",
-      separator_style = "slope",
-      show_buffer_close_icons = false,
-      show_close_icon = false,
-      always_show_bufferline = false,
-    },
-  }},
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
@@ -65,19 +50,4 @@ return {
       extensions = { "toggleterm", "nvim-tree" }
     end,
   },
-{
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("nvim-tree").setup {}
-  end,
-},
-{
-  "mbbill/undotree",
-  lazy = false
-},
 }
