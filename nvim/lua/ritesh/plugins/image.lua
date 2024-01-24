0379bb8b8ba3705ff2687b84c60d825390c4a53d
RiteshChepuri <riteshchepuri0@gmail.com>
2024-01-24 10:40:32 +0530

    feat: new formatter

--- nvim/lua/ritesh/plugins/image.lua
+++ nvim/lua/ritesh/plugins/image.lua
@@ -1,12 +1,12 @@
 return {
-  "HakonHarnes/img-clip.nvim",
-  event = "BufEnter",
-  opts = {
-    -- add options here
-    -- or leave it empty to use the default settings
-  },
-  keys = {
-    -- suggested keymap
-    { "<leader>cp", "<cmd>PasteImage<cr>", desc = "Paste clipboard image" },
-  },
+	"HakonHarnes/img-clip.nvim",
+	event = "BufEnter",
+	opts = {
+		-- add options here
+		-- or leave it empty to use the default settings
+	},
+	keys = {
+		-- suggested keymap
+		{ "<leader>cp", "<cmd>PasteImage<cr>", desc = "Paste clipboard image" },
+	},
 }
