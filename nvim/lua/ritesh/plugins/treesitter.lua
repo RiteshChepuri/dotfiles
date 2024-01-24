81204404f042530ac363703f4483269eba97f62a
RiteshChepuri <riteshchepuri0@gmail.com>
2024-01-24 10:43:43 +0530

    feat: added new formatter

--- nvim/lua/ritesh/plugins/treesitter.lua
+++ nvim/lua/ritesh/plugins/treesitter.lua
@@ -1,56 +1,56 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufreadPre", "BufNewFile" },
-    build = ":TSUpdate",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      "windwp/nvim-ts-autotag",
-    },
-    opts = {
-      ensure_installed = {
-        "bash",
-        "c",
-        "css",
-        "fish",
-        "gitignore",
-        "java",
-        "html",
-        "javascript",
-        "query",
-        "json",
-        "json5",
-        "lua",
-        "markdown",
-        "python",
-        "tsx",
-        "typescript",
-        "regex",
-        "markdown_inline",
-      },
-      sync_install = true,
-      rainbow = {
-        enable = true,
-        extended_mode = true,
-        max_files_lines = nil,
-      },
-      auto_install = true,
-      autotag = {
-        enable = true,
-      },
-      indent = {
-        enable = true,
-      },
-      highlight = {
-        enable = true,
-      },
-    },
-  },
-  {
-    "JoosepAlviste/nvim-ts-context-commentstring",
-    lazy = true,
-    opts = {
-      enable_autocmd = false,
-    },
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = { "BufreadPre", "BufNewFile" },
+		build = ":TSUpdate",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			"windwp/nvim-ts-autotag",
+		},
+		opts = {
+			ensure_installed = {
+				"bash",
+				"c",
+				"css",
+				"fish",
+				"gitignore",
+				"java",
+				"html",
+				"javascript",
+				"query",
+				"json",
+				"json5",
+				"lua",
+				"markdown",
+				"python",
+				"tsx",
+				"typescript",
+				"regex",
+				"markdown_inline",
+			},
+			sync_install = true,
+			rainbow = {
+				enable = true,
+				extended_mode = true,
+				max_files_lines = nil,
+			},
+			auto_install = true,
+			autotag = {
+				enable = true,
+			},
+			indent = {
+				enable = true,
+			},
+			highlight = {
+				enable = true,
+			},
+		},
+	},
+	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		lazy = true,
+		opts = {
+			enable_autocmd = false,
+		},
+	},
 }
