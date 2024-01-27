68d4e56e04699e1e39ef08c62a4cd1eaa56731eb
RiteshChepuri <riteshchepuri0@gmail.com>
2024-01-27 18:06:07 +0530

    feat: added boole plugin

---
+++ nvim/lua/ritesh/plugins/boole.lua
@@ -0,0 +1,22 @@
+return {
+	"nat-418/boole.nvim",
+	config = function()
+		require("boole").setup({
+			mappings = {
+				increment = "<C-a>",
+				decrement = "<C-x>",
+			},
+			additions = {
+				{ "enable", "disable" },
+				{ "production", "development", "test" },
+				{ "let", "const" },
+				{ "start", "end" },
+				{ "before", "after" },
+				{ "plus", "minus" },
+				{ "smart", "truncate" },
+				{ "left", "right" },
+				{ "top", "bottom" },
+			},
+		})
+	end,
+}
