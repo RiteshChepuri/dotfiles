aeacc9b027503cbaf5073050d4c46fe825b646ec
Ritesh Chepuri <riteshchepuri0@gmail.com>
2024-04-15 13:31:42 +0530

    chore: removed brew paths

--- fish/config.fish
+++ fish/config.fish
@@ -8,13 +8,6 @@ set -Ux fish_greeting
 # Set Neovim as default editor
 set -Ux EDITOR nvim
 
-# Homebrew
-eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
-test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-test -r ~/.bash_profile && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bash_profile
-echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.profile
-
 # Initialize starship
 starship init fish | source
 
