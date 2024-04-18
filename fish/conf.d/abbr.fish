fd03807e8a954a4561f1c51120c2425867571905
RiteshChepuri <riteshchepuri0@gmail.com>
2024-04-18 17:02:03 +0530

    feat: removed brew abbrs

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -9,14 +9,6 @@ abbr v. "nvim +GoToFile"
 abbr :GoToFile "nvim +GoToFile"
 abbr :GoToCommand "nvim +GoToCommand"
 
-# Brew
-abbr bc "brew cleanup"
-abbr bd "brew doctor"
-abbr bi "brew install"
-abbr bo "brew outdated"
-abbr bu "brew update"
-abbr bug "brew upgrade"
-
 # TMUX
 abbr ta "tmux a"
 abbr tk "tmux kill-server"
