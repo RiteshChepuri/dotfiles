62db80490428ac17d64f698905a7cd5e4b040053
RiteshChepuri <riteshchepuri0@gmail.com>
2024-04-28 21:08:33 +0530

    feat: removed update and clean functions from fish(migrated to arch)

--- fish/functions/clean.fish
+++
@@ -1,8 +0,0 @@
-function clean
-  sudo apt autoremove &&
-  echo "********************************** apt packages cleaning done **************************" &&
-  brew cleanup &&
-  echo "****************************************** brew cleanup done *************************************" &&
-  brew autoremove &&
-  echo "*********************************************************** brew autoremove done **********************************"
-end
