247f66a384a05b7c474e56c1f783b4a003331c53
RiteshChepuri <riteshchepuri0@gmail.com>
2024-05-25 14:52:13 +0530

    feat: changed trouble keymaps

--- nvim/lua/ritesh/plugins/trouble.lua
+++ nvim/lua/ritesh/plugins/trouble.lua
@@ -1,16 +1,25 @@
 return {
-	"folke/trouble.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		vim.keymap.set("n", "t]", function()
-			require("trouble").next({ skip_groups = true, jump = true })
-		end)
-		vim.keymap.set("n", "t[", function()
-			require("trouble").previous({ skip_groups = true, jump = true })
-		end)
-		vim.keymap.set("n", "<leader>tt", function()
-			require("trouble").toggle("workspace_diagnostics")
-		end)
-		vim.keymap.set("n", "<leader>td", vim.cmd.TodoTrouble)
-	end,
+    {
+        "folke/trouble.nvim",
+        lazy = false,
+        config = function()
+            require("trouble").setup({
+            })
+
+            vim.keymap.set("n", "<leader>tt", function()
+                require("trouble").toggle("workspace_diagnostics");
+            end)
+
+            vim.keymap.set("n", "t[", function()
+                require("trouble").next({skip_groups = true, jump = true});
+            end)
+
+            vim.keymap.set("n", "t]", function()
+                require("trouble").previous({skip_groups = true, jump = true});
+            end)
+
+            vim.keymap.set("n","<leader>td",vim.cmd.TodoTrouble)
+
+        end
+    },
 }
