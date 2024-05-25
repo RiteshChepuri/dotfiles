87611faf1685a4e7252db991aa9d551e4016ea72
RiteshChepuri <riteshchepuri0@gmail.com>
2024-05-25 14:59:06 +0530

    feat: added some misc plugins

--- nvim/lua/ritesh/plugins/misc.lua
+++ nvim/lua/ritesh/plugins/misc.lua
@@ -1,12 +1,12 @@
 return {
-	{ "nvim-lua/plenary.nvim" },
-	{ "christoomey/vim-tmux-navigator" },
-	{
-		"dstein64/vim-startuptime",
-		cmd = "StartupTime",
-	},
-	{
-		"stevearc/dressing.nvim",
-		opts = {},
-	},
+    { "nvim-lua/plenary.nvim" },
+    { "christoomey/vim-tmux-navigator" },
+    {
+        "dstein64/vim-startuptime",
+        cmd = "StartupTime",
+    },
+    {
+        "stevearc/dressing.nvim",
+        opts = {},
+    },
 }
