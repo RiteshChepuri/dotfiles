27e0078cfff956c5f06bf3c0e5126e882aa451c9
RiteshChepuri <riteshchepuri0@gmail.com>
2024-05-26 15:47:45 +0530

    feat: removed harpoon

--- nvim/lua/ritesh/plugins/telescope.lua
+++ nvim/lua/ritesh/plugins/telescope.lua
@@ -5,7 +5,6 @@ return {
 		{ "nvim-lua/plenary.nvim" },
 		{ "natecraddock/telescope-zf-native.nvim" },
 		{ "AckslD/nvim-neoclip.lua" },
-		{ "ThePrimeagen/harpoon" },
 		{ "nvim-lua/popup.nvim" },
 	},
 	config = function(_, opts)
@@ -13,7 +12,6 @@ return {
 		telescope.setup(opts)
 		telescope.load_extension("zf-native")
 		telescope.load_extension("neoclip")
-		telescope.load_extension("harpoon")
 		telescope.load_extension("noice")
 	end,
 	opts = {
@@ -38,49 +36,49 @@ return {
 		pickers = {
 			buffers = {
 				prompt_prefix = "󰸩 ",
-                initial_mode = "insert",
+				initial_mode = "insert",
 			},
 			commands = {
 				prompt_prefix = " ",
-                initial_mode = "insert",
+				initial_mode = "insert",
 			},
 			command_history = {
 				prompt_prefix = " ",
-                initial_mode = "insert",
+				initial_mode = "insert",
 			},
 			git_files = {
 				prompt_prefix = "󰊢 ",
 				show_untracked = true,
-                initial_mode = "insert",
+				initial_mode = "insert",
 			},
 			find_files = {
 				prompt_prefix = " ",
 				find_command = { "fd", "-H" },
-                initial_mode = "insert",
+				initial_mode = "insert",
 			},
 			live_grep = {
 				prompt_prefix = "󰱽 ",
-                initial_mode = "insert",
+				initial_mode = "insert",
 			},
 			grep_string = {
 				prompt_prefix = "󰱽 ",
-                initial_mode = "insert",
+				initial_mode = "insert",
 			},
 			keymaps = {
 				prompt_prefix = "󰌌 ",
-                initial_mode = "insert",
-            },
-        },
-    },
-    extensions = {
-        fzf = {
-            fuzzy = true, -- false will only do exact matching
-            override_generic_sorter = true, -- override the generic sorter
-            override_file_sorter = true, -- override the file sorter
-            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-        },
-    },
-    keys = function()
-        return {}
-    end,
+				initial_mode = "insert",
+			},
+		},
+	},
+	extensions = {
+		fzf = {
+			fuzzy = true, -- false will only do exact matching
+			override_generic_sorter = true, -- override the generic sorter
+			override_file_sorter = true, -- override the file sorter
+			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+		},
+	},
+	keys = function()
+		return {}
+	end,
 }
