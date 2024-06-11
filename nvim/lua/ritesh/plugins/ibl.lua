b3f222d9d26c7243613bda63cf3868184175b12f
RiteshChepuri <riteshchepuri0@gmail.com>
2024-06-11 15:15:49 +0530

    bot: formatter

--- nvim/lua/ritesh/plugins/ibl.lua
+++ nvim/lua/ritesh/plugins/ibl.lua
@@ -1,8 +1,9 @@
 return {
-    { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {},
-    context_char = "│",
-    config = function ()
-        require("ibl").setup()
-    end
-}
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	opts = {},
+	context_char = "│󰇝",
+	config = function()
+		require("ibl").setup()
+	end,
 }
