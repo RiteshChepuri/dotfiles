de8d3554a161a56569f0eb3e6c8f74a20f076279
RiteshChepuri <riteshchepuri0@gmail.com>
2024-06-16 21:21:11 +0530

    change: changed the go path to sesh path

--- fish/config.fish
+++ fish/config.fish
@@ -15,7 +15,7 @@ starship init fish | source
 zoxide init fish | source
 
 fish_add_path -m ~/.config/local/bin
-fish_add_path -m ~/go/bin
+fish_add_path -m ~/go/bin/sesh
 fish_add_path -m ~/.local/share/gem/ruby/3.0.0/bin
 fish_add_path -m ~/.cargo/bin/
 fish_add_path -m ~/.local/bin/
