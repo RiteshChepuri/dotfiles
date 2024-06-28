a46ae8a5bed398f8d95fddde08b89b53250581ee
Ritesh Chepuri <riteshchepuri0@gmail.com>
2024-06-28 17:46:41 +0530

    feat: added undo highlight plugin

--- nvim/lua/plugins/undotree.lua
+++ nvim/lua/plugins/undotree.lua
@@ -1,7 +1,14 @@
 return {
-	"mbbill/undotree",
-    config = function()
-
-vim.keymap.set("n", "<leader>u", "<Cmd>UndotreeToggle<CR>")
-    end
+	{
+		"mbbill/undotree",
+		config = function()
+			vim.keymap.set("n", "<leader>u", "<Cmd>UndotreeToggle<CR>")
+		end,
+	},
+	{
+		"tzachar/highlight-undo.nvim",
+		opts = {
+			...,
+		},
+	},
 }
