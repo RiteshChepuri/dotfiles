a11d8ea456e9efa2c8d0bf4c9eba1f0e33fbf612
Ritesh Chepuri <riteshchepuri0@gmail.com>
2024-06-28 17:40:13 +0530

    feat: added a lua function to create a command that toggles git diff

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -93,3 +93,15 @@ vim.api.nvim_create_user_command("OilToggle", function()
 		vim.cmd("Oil")
 	end
 end, { nargs = 0 })
+
+local my_diffview_toggle = function()
+	local lib = require("diffview.lib")
+	local view = lib.get_current_view()
+	if view then
+		-- Current tabpage is a Diffview; close it
+		vim.cmd.DiffviewClose()
+	else
+		-- No open Diffview exists: open a new one
+		vim.cmd.DiffviewOpen()
+	end
+end
