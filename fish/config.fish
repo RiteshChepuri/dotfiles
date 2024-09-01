4837306576ce2ad473dc5f9564986bdda8b45b58
RiteshChepuri <riteshchepuri0@gmail.com>
2024-09-01 20:42:23 +0530

    feat: removed unneccesary files

--- fish/config.fish
+++ fish/config.fish
@@ -8,25 +8,13 @@ set -Ux fish_greeting
 # Set Neovim as default editor
 set -Ux EDITOR nvim
 
-# Initialize starship
+# Intialize starship prompt
 starship init fish | source
 
-# Initialize zoxide
+# Intialize zoxide
 zoxide init fish | source
 
+# env paths
+fish_add_path -m ~/.local/share/gem/ruby/3.2.0/bin
 fish_add_path -m ~/.config/local/bin
-fish_add_path -m ~/go/bin/sesh
-fish_add_path -m ~/.local/share/gem/ruby/3.0.0/bin
-fish_add_path -m ~/.cargo/bin/
-fish_add_path -m ~/.local/bin/
-
-
-# pnpm
-set -gx PNPM_HOME "/home/ritesh/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
-
-# Set up fzf key bindings
-fzf --fish | source
+fish_add_path -m /usr/local/go/bin
