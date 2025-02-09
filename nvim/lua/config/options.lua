801d995aec46922760f71bff00d04ca8367abb68
RiteshChepuri <riteshchepuri0@gmail.com>
2025-02-09 19:07:58 +0530

    feat: fixes in options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -44,9 +44,5 @@ vim.opt.fileencoding = "utf-8"
 -- cursor line
 vim.opt.cursorline = true
 
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
