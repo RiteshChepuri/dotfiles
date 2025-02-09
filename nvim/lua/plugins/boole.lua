88ced7a0dbb0d3e8fbadb43181a64026244a085b
RiteshChepuri <riteshchepuri0@gmail.com>
2025-02-09 19:08:19 +0530

    feat: added boole plugin

--- nvim/lua/plugins/boole.lua
+++ nvim/lua/plugins/boole.lua
@@ -1,23 +1,25 @@
 return {
 	"nat-418/boole.nvim",
-	config = function()
-		require("boole").setup({
-			mappings = {
-				increment = "<C-a>",
-				decrement = "<C-x>",
-			},
-			additions = {
-				{ "enable", "disable" },
-				{ "production", "development", "test" },
-				{ "let", "const" },
-				{ "start", "end" },
-				{ "before", "after" },
-				{ "plus", "minus" },
-				{ "smart", "truncate" },
-				{ "left", "right" },
-				{ "top", "bottom" },
-				{ "is", "are" },
-			},
-		})
-	end,
+	event = "BufEnter",
+	opts = {
+		mappings = {
+			increment = "<C-a>",
+			decrement = "<C-x>",
+		},
+		-- NOTE: a lot of the toggles are taken from josh medeski
+		additions = {
+			{ "production", "development", "test", "sandbox" },
+			{ "let", "const" },
+			{ "start", "end" },
+			{ "import", "export" },
+			{ "before", "after" },
+			{ "plus", "minus" },
+			{ "smart", "truncate" },
+			{ "left", "right" },
+			{ "is", "are" },
+			{ "enable", "disable" },
+			{ "top", "bottom" },
+			{ "foregound", "background" },
+		},
+	},
 }
