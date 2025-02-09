d724fbb275828173df635dc703c489be23681475
RiteshChepuri <riteshchepuri0@gmail.com>
2025-02-09 19:07:28 +0530

    feat: fixes in keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,24 +6,12 @@ vim.keymap.set("i", "<C-c>", "<ESC>", { desc = "Better Escape" })
 vim.keymap.set("n", "<A-o>", "o<ESC>")
 vim.keymap.set("n", "<A-O>", "O<ESC>")
 
--- Open Netrw
-vim.keymap.set("n", "<leader>e", "<Cmd>NvimTreeToggle<CR>")
-
--- Close a Buffer
-vim.keymap.set("n", "<A-e>", vim.cmd.bd)
-
 -- Clear searches
 vim.keymap.set("n", "<ESC>", "<Cmd>noh<CR><ESC>")
-
--- Delete a character without copying into the register
-vim.keymap.set("n", "x", '"_x')
 
 -- Scroll Page down (Center the cursor)
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
-
--- Make the Current file executable
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR> <CR>", { silent = true })
 
 -- Disable Space
 vim.keymap.set("n", "<Space>", "<NOP>")
