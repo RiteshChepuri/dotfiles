dff2c0aa57392ae1fc11e6866ca7d30f781d40b4
RiteshChepuri <riteshchepuri0@gmail.com>
2025-02-23 08:58:29 +0530

    fix: fixed a warning about unpack

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -21,7 +21,7 @@ api.nvim_create_autocmd("BufWritePre", {
 vim.api.nvim_create_autocmd("BufReadPost", {
 	pattern = "*",
 	callback = function()
-		local row, col = unpack(vim.api.nvim_buf_get_mark(0, '"'))
+		local row, col = table.unpack(vim.api.nvim_buf_get_mark(0, '"'))
 		if row > 0 and row <= vim.api.nvim_buf_line_count(0) then
 			vim.api.nvim_win_set_cursor(0, { row, col })
 		end
