dc7ef4f7a8744456b1d57cfe4688a4ed751bdea9
RiteshChepuri <riteshchepuri0@gmail.com>
2025-03-10 07:52:11 +0530

    feat: added few lsp servers and configured

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -10,15 +10,22 @@ return {
 		config = function()
 			require("mason-lspconfig").setup({
 				ensure_installed = {
-					"lua_ls",
-					"emmet_ls",
-					"tailwindcss",
-					"ts_ls",
-					"cssls",
-					"clangd",
-					"pyright",
-					"phpactor",
-					"bashls",
+					"clangd", -- c
+					"basedpyright", -- python
+					"intelephense", -- php
+					"bashls", -- bash
+					"jdtls", -- java
+					"jsonls", -- json
+					"lua_ls", -- lua
+					"superhtml", -- html
+					"html", -- html
+					"cssls", -- css
+					"css_variables", -- css
+					"cssmodules_ls", -- css
+					"marksman", -- markdown
+					"emmet_ls", -- js,jsx,ts,tsx,html,css,scss,sass
+					"tailwindcss", -- tailwindcss
+					"ts_ls", -- javascript, typescript
 				},
 			})
 		end,
@@ -27,6 +34,7 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			local capabilities = require("blink.cmp").get_lsp_capabilities()
+			capabilities.textDocument.completion.completionItem.snippetSupport = true
 			local lspconfig = require("lspconfig")
 			local keymap = vim.keymap
 			local opts = { noremap = true, silent = true }
@@ -52,45 +60,20 @@ return {
 				keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- next diagnostics
 			end
 
-			-- configure emmet language server
-			lspconfig.emmet_ls.setup({
-				capabilities = capabilities,
-				on_attach = on_attach,
-				filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
-			})
-
-			-- configure typescript server for typescript
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-				on_attach = on_attach,
-			})
-
-			-- configure css server for css
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-				on_attach = on_attach,
-			})
-
-			-- configure tailwindcss server
-			lspconfig.tailwindcss.setup({
-				capabilities = capabilities,
-				on_attach = on_attach,
-			})
-
 			-- configure clangd server for c language
 			lspconfig.clangd.setup({
 				capabilities = capabilities,
 				on_attach = on_attach,
 			})
 
-			-- configure pyright server for python
-			lspconfig.pyright.setup({
+			-- configure basedpyright server for python
+			lspconfig.basedpyright.setup({
 				capabilities = capabilities,
 				on_attach = on_attach,
 			})
 
-			-- configure phpactor server for php
-			lspconfig.phpactor.setup({
+			-- configure intelephense server for php
+			lspconfig.intelephense.setup({
 				capabilities = capabilities,
 				on_attach = on_attach,
 			})
@@ -99,6 +82,59 @@ return {
 			lspconfig.bashls.setup({
 				capabilities = capabilities,
 				on_attach = on_attach,
+			})
+
+			-- configure jdtls server for java
+			lspconfig.jdtls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+
+			-- configure jsonls server for json
+			lspconfig.jsonls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+
+			-- configure marksman server for markdown
+			lspconfig.marksman.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+
+			-- configure super-html server for html
+			lspconfig.superhtml.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				filetypes = { "html" },
+			})
+
+			-- configure html server for html
+			lspconfig.html.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				filetypes = { "html" },
+			})
+
+			-- configure css server for css
+			lspconfig.cssls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				filetypes = { "css", "scss", "sass" },
+			})
+
+			-- configure css_variables server for css
+			lspconfig.css_variables.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				filetypes = { "css", "scss", "sass" },
+			})
+
+			-- configure cssmodules_ls server for css
+			lspconfig.cssmodules_ls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				filetypes = { "css", "scss", "sass" },
 			})
 
 			-- configure luals server for lua language
@@ -119,6 +155,44 @@ return {
 							},
 						},
 					},
+				},
+			})
+
+			-- configure tailwindcss server for tailwind
+			lspconfig.tailwindcss.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+
+			-- configure emmet language server
+			lspconfig.emmet_ls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				filetypes = {
+					"javascript",
+					"javascriptreact",
+					"typescript",
+					"typescriptreact",
+					"javascript.jsx",
+					"typescript.tsx",
+					"html",
+					"css",
+					"scss",
+					"sass",
+				},
+			})
+
+			-- configure typescript server for typescript
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				filetypes = {
+					"javascript",
+					"javascriptreact",
+					"typescript",
+					"typescriptreact",
+					"javascript.jsx",
+					"typescript.tsx",
 				},
 			})
 		end,
