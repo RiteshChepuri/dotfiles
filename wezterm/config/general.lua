feada0f82b554916e8eb5ad562b01f62080af530
RiteshChepuri <riteshchepuri0@gmail.com>
2025-03-26 21:15:03 +0530

    feat: changed font size

--- wezterm/config/general.lua
+++ wezterm/config/general.lua
@@ -3,7 +3,7 @@ local M = {}
 
 function M.apply(config)
 	config.font = wezterm.font("JetBrainsMono")
-	config.font_size = 13.0
+	config.font_size = 10.0
 	config.color_scheme = "Catppuccin Mocha"
 	config.window_background_opacity = 0.55
 	config.adjust_window_size_when_changing_font_size = false
