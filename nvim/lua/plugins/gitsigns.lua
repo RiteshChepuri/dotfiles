85312a67fb738325fff46812e87ae524b8f189d5
RiteshChepuri <riteshchepuri0@gmail.com>
2025-03-30 18:01:55 +0530

    feat: changed icons for git siogns

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -5,22 +5,10 @@ return {
 	config = function()
 		require("gitsigns").setup({
 			signs = {
-				add = { text = "┃" },
-				change = { text = "┃" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-				untracked = { text = "┆" },
+				add = { text = "" },
+				change = { text = "" },
+				delete = { text = "" },
 			},
-			signs_staged = {
-				add = { text = "┃" },
-				change = { text = "┃" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-				untracked = { text = "┆" },
-			},
-			signs_staged_enable = true,
 			signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
 			numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
 			linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
