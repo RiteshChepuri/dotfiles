0c0dbbafc2f6201f3b6a581ff4d541532ea0dd3a
RiteshChepuri <riteshchepuri0@gmail.com>
2025-08-04 15:40:26 +0530

    feat: removed latex from treesitter neovim

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -35,7 +35,6 @@ return {
                     "php",
                     "astro",
                     "regex",
-                    "latex",
                     "norg",
                     "scss",
                     "svelte",
