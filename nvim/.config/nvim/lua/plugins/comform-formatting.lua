6f7ed55b067de2f7979644e79c19aec185c160a0
RiteshChepuri <riteshchepuri0@gmail.com>
2025-11-22 10:13:38 +0530

    feat: added langs for formatting for neovim

--- nvim/.config/nvim/lua/plugins/comform-formatting.lua
+++ nvim/.config/nvim/lua/plugins/comform-formatting.lua
@@ -7,6 +7,20 @@ return {
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
+				python = { "isort", "black" },
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettierd" },
+				javascriptreact = { "prettierd" },
+				typescriptreact = { "prettierd" },
+				html = { "prettierd" },
+				css = { "prettierd" },
+				yaml = { "prettierd" },
+				markdown = { "prettierd" },
+				json = { "prettierd" },
+				rust = { "rustfmt", lsp_format = "fallback" },
+				sh = { "beautysh" },
+				c = { "clang-format" },
+				java = { "google-java-format" },
 			},
 			format_on_save = {
 				lsp_fallback = true,
